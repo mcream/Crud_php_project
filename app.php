f310a750e78963e2602267eb0c646ea52897cf4a
travis <travis@travis-ci.org>
2019-09-02 18:04:59 +0200

    added all backend logic for index.php. Added session and connect datas app.php with index

--- app.php
+++ app.php
@@ -7,4 +7,29 @@ $database = mysqli_connect('localhost', 'root', '', 'project_php');
 	$fname = "";
 	$lname = "";
 	$id = 0;
-	$update = false;
+    $update = false;
+    
+    if (isset($_POST['save'])) {
+		$fname = $_POST['fname'];
+		$lname = $_POST['lname'];
+
+		mysqli_query($database, "INSERT INTO db (fname, lname) VALUES ('$fname', '$lname')");
+		$_SESSION['message'] = "Record has been saved";
+		header('location: index.php');
+	}
+
+  if (isset($_POST['update'])) {
+  	$id = $_POST['id'];
+  	$fname = $_POST['fname'];
+  	$lname = $_POST['lname'];
+
+  	mysqli_query($database, "UPDATE db SET name='$name', lname='$lname' WHERE id=$id");
+  	$_SESSION['message'] = "Record has been Updated";
+  	header('location: index.php');
+  }
+  if (isset($_GET['del'])) {
+  	$id = $_GET['del'];
+  	mysqli_query($database, "DELETE FROM db WHERE id=$id");
+  	$_SESSION['message'] = "Record has been deleted";
+  	header('location: index.php');
+  }
--- index.php
+++ index.php
@@ -1,17 +1,43 @@
 <?php 
     require_once('app.php');
+	if (isset($_GET['edit'])) {
+		$id = $_GET['edit'];
+		$update = true;
+		$record = mysqli_query($database, "SELECT * FROM info WHERE id=$id");
 
+		if (count($record) == 1 ) {
+			$n = mysqli_fetch_array($record);
+			$lname = $n['lname'];
+			$fname = $n['fname'];
+		}
+	}
 ?>
 <!doctype html>
 <html>
 <head>
     <title>CRUD by PHP</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        .request{
+            margin: 0;
+            padding: 20px;
+            color: #666;
+            background: #eee;
+            width: 100%;
+            text-align: center;
+        }
+    
+    </style>
 </head>
 <body>
 <!-- Container. Take all app in one place -->
     <!-- Table with added records to dbs -->
     <div class="conatiner">
+        <div class="col-xl-12 request">
+            <?php
+            ?>
+        </div>
+    
             <table class="table">
         <thead class="thead-light">
             <tr>
@@ -50,10 +76,12 @@
             <div class="form-group">
                 <label for="lastname">Last name</label>
                 <input type="text" class="form-control" name="lname" placeholder="Enter last name">
-            </div>
-
+            </div>  
+            <?php if ($update == true): ?>
             <button type="submit" class="btn btn-success" name="upadate">Add and Edit</button>
+            <?php else: ?>
             <button type="submit" class="btn btn-danger" name="save">Delete</button>
+            <?php else: ?>
         </form>
     </div>
     
