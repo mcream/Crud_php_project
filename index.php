d3b57a52e545357f255a67cb3af39e702b1bc031
travis <travis@travis-ci.org>
2019-09-02 13:15:51 +0200

    added table with example records

--- index.php
+++ index.php
@@ -1,8 +1,45 @@
+<!doctype html>
 <html>
 <head>
     <title>CRUD by PHP</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+    <div class="conatiner">
+            <table class="table">
+        <thead class="thead-light">
+            <tr>
+            <th scope="col">Number</th>
+            <th scope="col">First name</th>
+            <th scope="col">Last name</th>
+            <th scope="col">Option</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
+        </table>
+
+      
+    </div>
+    
+
 </body>
 </html>
