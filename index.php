2ec912db52babfab3aced41ff3ea8bc57035dbcc
travis <travis@travis-ci.org>
2019-09-02 13:37:13 +0200

    created 'add form'. Through this form, We'll adding our records

--- index.php
+++ index.php
@@ -5,39 +5,51 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+<!-- Container. Take all app in one place -->
+    <!-- Table with added records to dbs -->
     <div class="conatiner">
             <table class="table">
         <thead class="thead-light">
             <tr>
-            <th scope="col">Number</th>
+            <!-- described columns -->
             <th scope="col">First name</th>
             <th scope="col">Last name</th>
             <th scope="col">Option</th>
             </tr>
         </thead>
+        <!-- records -->
         <tbody>
             <tr>
-            <th scope="row">1</th>
             <td>Mark</td>
             <td>Otto</td>
-            <td>@mdo</td>
+            <td></td>
             </tr>
             <tr>
-            <th scope="row">2</th>
             <td>Jacob</td>
             <td>Thornton</td>
-            <td>@fat</td>
+            <td></td></td>
             </tr>
             <tr>
-            <th scope="row">3</th>
             <td>Larry</td>
             <td>the Bird</td>
-            <td>@twitter</td>
+            <td></td>
             </tr>
         </tbody>
         </table>
+        <form action="">
+        <!-- Form for adding new records -->
+            <div class="form-group">
+                <label for="firstname">First name</label>
+                <input type="text" class="form-control" placeholder="Enter first name">
+            </div>
+            <div class="form-group">
+                <label for="lastname">Last name</label>
+                <input type="text" class="form-control" placeholder="Enter last name">
+            </div>
 
-      
+            <button type="submit" class="btn btn-success">Add and Edit</button>
+            <button type="submit" class="btn btn-danger">Delete</button>
+        </form>
     </div>
     
 
